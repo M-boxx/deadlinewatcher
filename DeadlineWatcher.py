ffab603509e2da002a70205c5aa6e7180a0ef9a5
M-boxx <m.boxix@mail.ru>
2021-12-15 19:03:17 +0300

    Bot has been redeveloped on Telebot, added buttons

--- DeadlineWatcher.py
+++ DeadlineWatcher.py
@@ -9,98 +9,92 @@ DATE_DAY = 2
 
 bot = telebot.TeleBot('5012802026:AAGeqsv5Qi2TrWmRcAz9anKDudxAQiWeAow')
 
-# функция, отвечающая за начало программы
-def start(update, context):
+deadline_data = {}
 
-    update.message.reply_text('40 Приветствую! Я - бот, следящий за вашими дедлайнами')
+@bot.message_handler(commands=['start'])
+def start_message(message):
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    buttonget = types.KeyboardButton('Установить дедлайн')
+    buttonshow = types.KeyboardButton('Посмотреть дедлайн')
+    markup.add(buttonget)
+    markup.add(buttonshow)
+    bot.send_message(message.chat.id, '51 Приветствую! Я - бот, следящий за вашими дедлайнами\nЕсли вы хотите установить новый дедлайн используйте кнопку "Установить дедлайн", для просмотра дедлайна используй "Посмотреть дедлайн"', reply_markup=markup)
+
     
-    get_deadline_date(update, context)
 
 # функция, отвечающая за получения даты дедлайна
-def get_deadline_date(update, context):
+def get_deadline_date(message):
     global SITUATION
     SITUATION = DATE_MONTH
-    update.message.reply_text('Позвольте узнать, к какому по счёту месяцу вы хотите закончить работу?')
+    bot.send_message(message.chat.id, 'Позвольте узнать, к какому по счёту месяцу вы хотите закончить работу?')
 
 
 # функция, отвечающая за команду помощи
-def help(update, context):
-    update.message.reply_text('help command received')
+def help(message):
+    bot.send_message(message.chat.id, 'help command received')
 
 # функция, отвечающая за ошибку, возникшую во время получения данных
-def error(update, context):
-    update.message.reply_text('an error occured')
+def error(message):
+    bot.send_message(message.chat.id, 'об Астралис')
 
-# function to handle normal text 
-def text(update, context):
-    text_received = update.message.text
+@bot.message_handler(content_types=["text"])
+def text(message):
+    text_received = message.text
     
+    if text_received=='Установить дедлайн':
+        return get_deadline_date(message)
+    if text_received=='Посмотреть дедлайн':
+        return deadline_check(message)
+
+
     global SITUATION
-
     if SITUATION==DATE_MONTH:
-        return received_deadline_month(update, context)
+        return received_deadline_month(message)
     if SITUATION==DATE_DAY:
-        received_deadline_day(update, context)
+        return received_deadline_day(message)
 
-def received_deadline_month(update, context):
+def received_deadline_month(message):
     global SITUATION
 
     try:
-        text_received = update.message.text
+        text_received = message.text
         month = int(text_received)
 
         if month>12 or month<1:
             raise ValueError('Такого месяца не может быть')
-        context.user_data['deadline_month'] = month
-        update.message.reply_text('Прекрасно, теперь введите число этого месяца')
+        deadline_data[f'{message.chat.id}deadline_month'] = month
+        bot.send_message(message.chat.id, 'Прекрасно, теперь введите число этого месяца')
         SITUATION = DATE_DAY
     except:
-        update.message.reply_text('Неправильный формат ввода')
+        bot.send_message(message.chat.id, 'Неправильный формат ввода')
 
-def received_deadline_day(update, context):
+def received_deadline_day(message):
     global SITUATION
 
     try:
-        text_received = update.message.text
+        text_received = message.text
         day = int(text_received)
 
         if day>31 or day<1:
             raise ValueError('Такого дня быть не может')
-        context.user_data['deadline_day'] = day
-        update.message.reply_text('Дедлайн установлен')
+        deadline_data[f'{message.chat.id}deadline_day'] = day
+        bot.send_message(message.chat.id, 'Дедлайн установлен')
         SITUATION = None
     except:
-        update.message.reply_text('Неправильный формат ввода')
+        bot.send_message(message.chat.id, 'Неправильный формат ввода')
 
-def deadline(update, context):
+def deadline_check(message):
     now = datetime.datetime.now()
-    update.message.reply_text(now.day)
-    update.message.reply_text(now.month)
-
-def main():
-    TOKEN = "5012802026:AAGeqsv5Qi2TrWmRcAz9anKDudxAQiWeAow"
-
-    # create the updater, that will automatically create also a dispatcher and a queue to 
-    # make them dialoge
-    updater = Updater(TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-
-    # add handlers for start and help commands
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help))
-    dispatcher.add_handler(CommandHandler("deadline", deadline))
-
-    # add an handler for normal text (not commands)
-    dispatcher.add_handler(MessageHandler(Filters.text, text))
-
-    # add an handler for errors
-    dispatcher.add_error_handler(error)
-
-    # start your shiny new bot
-    updater.start_polling()
-
-    # run the bot until Ctrl-C
-    updater.idle()
+    userid = message.chat.id
+    #bot.send_message(message.chat.id, now.day)
+    #bot.send_message(message.chat.id, now.month)
+    try:
+        saved_day = deadline_data[f'{userid}deadline_day']
+        saved_month = deadline_data[f'{userid}deadline_month']
+        bot.send_message(userid, f'Дедлайн назначен на {saved_day}.{saved_month}!')
+    except:
+        bot.send_message(userid, 'Дедлайн не был установлен ранее')
 
 if __name__ == '__main__':
-    main()
+    bot.infinity_polling()
