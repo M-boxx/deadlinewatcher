8cebf09f148fe1c547cb9a1a5fb23f8b94646ded
M-boxx <m.boxix@mail.ru>
2021-12-15 13:48:17 +0300

    Version 40

--- DeadlineWatcher.py
+++ DeadlineWatcher.py
@@ -1,24 +1,81 @@
 # DeadlineWatcher
+import telebot
+from telebot import types 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import datetime
+SITUATION = None
+DATE_MONTH = 1
+DATE_DAY = 2
 
-# function to handle the /start command
+bot = telebot.TeleBot('5012802026:AAGeqsv5Qi2TrWmRcAz9anKDudxAQiWeAow')
+
+# функция, отвечающая за начало программы
 def start(update, context):
-    update.message.reply_text('Приветствую! Я - бот, следящий за вашими дедлайнами \n К какому числу')
+
+    update.message.reply_text('40 Приветствую! Я - бот, следящий за вашими дедлайнами')
+    
+    get_deadline_date(update, context)
+
+# функция, отвечающая за получения даты дедлайна
+def get_deadline_date(update, context):
+    global SITUATION
+    SITUATION = DATE_MONTH
+    update.message.reply_text('Позвольте узнать, к какому по счёту месяцу вы хотите закончить работу?')
 
 
-
-# function to handle the /help command
+# функция, отвечающая за команду помощи
 def help(update, context):
     update.message.reply_text('help command received')
 
-# function to handle errors occured in the dispatcher 
+# функция, отвечающая за ошибку, возникшую во время получения данных
 def error(update, context):
     update.message.reply_text('an error occured')
 
 # function to handle normal text 
 def text(update, context):
     text_received = update.message.text
-    update.message.reply_text(f'did you said "{text_received}" ?')
+    
+    global SITUATION
+
+    if SITUATION==DATE_MONTH:
+        return received_deadline_month(update, context)
+    if SITUATION==DATE_DAY:
+        received_deadline_day(update, context)
+
+def received_deadline_month(update, context):
+    global SITUATION
+
+    try:
+        text_received = update.message.text
+        month = int(text_received)
+
+        if month>12 or month<1:
+            raise ValueError('Такого месяца не может быть')
+        context.user_data['deadline_month'] = month
+        update.message.reply_text('Прекрасно, теперь введите число этого месяца')
+        SITUATION = DATE_DAY
+    except:
+        update.message.reply_text('Неправильный формат ввода')
+
+def received_deadline_day(update, context):
+    global SITUATION
+
+    try:
+        text_received = update.message.text
+        day = int(text_received)
+
+        if day>31 or day<1:
+            raise ValueError('Такого дня быть не может')
+        context.user_data['deadline_day'] = day
+        update.message.reply_text('Дедлайн установлен')
+        SITUATION = None
+    except:
+        update.message.reply_text('Неправильный формат ввода')
+
+def deadline(update, context):
+    now = datetime.datetime.now()
+    update.message.reply_text(now.day)
+    update.message.reply_text(now.month)
 
 def main():
     TOKEN = "5012802026:AAGeqsv5Qi2TrWmRcAz9anKDudxAQiWeAow"
@@ -31,6 +88,7 @@ def main():
     # add handlers for start and help commands
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help))
+    dispatcher.add_handler(CommandHandler("deadline", deadline))
 
     # add an handler for normal text (not commands)
     dispatcher.add_handler(MessageHandler(Filters.text, text))
